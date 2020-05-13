ce53506e80ebddd358e7e2dd5efcfd77602cdfe7
ErikaN-ir <erika.nethaway@gmail.com>
2020-05-13 20:14:08 +0000

    Added html files

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask #importing flask class
+from flask import Flask, render_template #importing flask class
 
 app = Flask(__name__) #Creating an instance of it & storing it in app
 #__name__ is the name of the module - built-in python variable
+
 @app.route("/") #wraps a function
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 if __name__ == "__main__": #name of default module
     app.run(host=os.environ.get("IP"),
