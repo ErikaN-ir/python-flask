158b4837e5a8ddb3ccd86f7f8b78be3d73dbc1b8
ErikaN-ir <erika.nethaway@gmail.com>
2020-05-17 11:02:49 +0000

    More routing

--- run.py
+++ run.py
@@ -8,6 +8,14 @@ app = Flask(__name__) #Creating an instance of it & storing it in app
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__": #name of default module
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
