9134a6225a058353eb38819d11e9efea29236700
ErikaN-ir <erika.nethaway@gmail.com>
2020-05-21 21:28:53 +0000

    Added display message on submitted form for contact page

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 import json
-from flask import Flask, render_template #importing flask class
+from flask import Flask, render_template, request, flash #importing flask class
 
 app = Flask(__name__) #Creating an instance of it & storing it in app
 #__name__ is the name of the module - built-in python variable
+app.secret_key = "some_secret"
 
 @app.route("/") #wraps a function
 def index():
@@ -28,8 +29,11 @@ def about_member(member_name):
     
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
+
     return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers.html")
